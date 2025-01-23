21eaaab0957681f6527813b33f1c887e06c20bcf
damianloch <damian.loch@arvoai.ca>
2025-01-23 10:00:35 -0500

    minor changes

--- app/app.py
+++ app/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
 
 @app.route("/")
 def index():
@@ -11,4 +13,4 @@ def get_message():
     return jsonify(message="Hello, World!")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="127.0.0.1", port=5000)
